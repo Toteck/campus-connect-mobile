b2b6a9e3135cbda260650c72d22685b9e260e29f
mateus <mateusweslley14@gmail.com>
2024-08-19 09:46:06 -0300

    adicionando novamente o SplashScreen

--- src/app/(tabs)/myevents.tsx
+++ src/app/(tabs)/myevents.tsx
@@ -6,13 +6,7 @@ import { Text, View } from "../../components/Themed";
 export default function SearchScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Inicio</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Meus Eventos</Text>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -52,6 +52,12 @@ export default function RootLayout() {
     if (error) throw error;
   }, [error]);
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   if (!loaded) {
     return <Loading />;
   }
